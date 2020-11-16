a0e3af052f703ae99f4656321e0e42aa5782175d
Maguilera85 <drianketing@gmail.com>
2020-11-16 12:39:45 -0300

    algunos avances en agregar cliente

--- finanzas_miguel.py
+++ finanzas_miguel.py
@@ -12,16 +12,21 @@ for i in range(3):
 # limitar cantidad max de clientes y monto disponible para transferencias desde saldo_institucional
 # 
 
+
+#------------> HACER: transferir y giros totales <------------#
+
+
 class Cliente():
         
-    def __init__(self, nombre, id_cliente, saldo):
-        self.nombre = nombre
-        self.id_cliente = id_cliente
-        self.saldo = saldo
-    
-    def info_cliente(self):
-        self.info_lista = []
 
+    def __init__(self, nombre, saldo):
+        self.nombre = nombre
+        self.id_cliente = uuid.uuid4()
+        self.saldo = saldo
+        self.datos_cliente = []
+        self.datos_cliente.append(nombre)
+        self.datos_cliente.append(self.id_cliente)
+        self.datos_cliente.append(saldo)
 
     def girar(self):
         pass
@@ -32,27 +37,22 @@ class Cliente():
     def mostrar_saldo(self):
         pass
 
-    def generador_id(self): # Generador id funcionando ok 
-        self.id_cliente = uuid.uuid4()
-        return self.id_cliente
+
 
 
 class Financiera():
 
-    def __init__(self, nombre, id_financiera, saldo_institucional, clientes):
+    base_clientes: {}
+    total_clientes = 0
+
+    def __init__(self, nombre, id_financiera, saldo_institucional):
         self.nombre = nombre
-        self.id_financiera = id_financiera
+        self.id_financiera = uuid.uuid4()
         self.saldo_institucional = saldo_institucional
         self.clientes = []
-
-    def agregar_cliente(self): # verificar que se cumple condicion del 10 % sino rechazar
         
-        for _cliente in self.clientes: # Indica cantidad de cliente en la financiera
-            contador = 0
-            contador += 1
-            _total_clientes = contador
-        
-
+    def agregar_cliente(self, *lista): # verificar que se cumple condicion del 10 % sino rechazar
+        self.datos_cliente= lista # guarda informacion cliente   
         self.linea_credito = 1000000  # por un cliente
         self.lim_max_cred = self.saldo_institucional * 0.10 # se establece max de linea de credito
         self.acceso_credito = 0
@@ -74,36 +74,25 @@ class Financiera():
     def mostrar_saldo_institucional(self):
         pass
 
-    def generador_id(self): # Generador id funcionando ok 
-        self.id_financiera = uuid.uuid4()
-        return self.id_financiera
+
 
 
 # Creando financieras
-pasa_todo = Financiera("Pasa Todo", "", 100000000, "")
-sin_niuno = Financiera("Sin niuno", "", 100000000, "")
+pasa_todo = Financiera("Pasa Todo", "", 100000000)
+sin_niuno = Financiera("Sin niuno", "", 100000000)
 
 # Creando cliente (4 x Financiera)
-cliente_a1 = Cliente("Seba", "", 1000000)
-cliente_a2 = Cliente("Walter", "", 1000000)
-cliente_a3 = Cliente("Claudio", "", 1000000)
-cliente_a4 = Cliente("Miguel", "", 1000000)
+cliente_a1 = Cliente("Seba", 1000000)
+pasa_todo.agregar_cliente(cliente_a1.datos_cliente) # se agrega el cliente en lista de financiera
 
-cliente_b1 = Cliente("Jonathan", "", 1000000)
-cliente_b2 = Cliente("Luis", "", 1000000)
-cliente_b3 = Cliente("Gloria", "", 1000000)
-cliente_b4 = Cliente("Vania", "", 1000000)
+cliente_a2 = Cliente("Walter", 1000000)
+cliente_a3 = Cliente("Claudio", 1000000)
+cliente_a4 = Cliente("Miguel", 1000000)
 
-
-
-
-print("Numero de clientes en financiera: ", pasa_todo.clientes)
-print(Cliente.__init__)
-
-
-
-
-
+cliente_b1 = Cliente("Jonathan", 1000000)
+cliente_b2 = Cliente("Luis", 1000000)
+cliente_b3 = Cliente("Gloria", 1000000)
+cliente_b4 = Cliente("Vania", 1000000)
 
 
 
@@ -112,5 +101,4 @@ print(Cliente.__init__)
 
 
 if __name__ == "__main__":
-    cliente_prueba = Cliente("cliente_prueba", "", 1000)
-    print(cliente_prueba.generador_id())
+    pass
